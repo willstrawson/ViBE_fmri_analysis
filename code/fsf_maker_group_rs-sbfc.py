935694b4b6253da63156f4ee1cafa86c31c5488d
Will Strawson <willstrawson@Wills-MacBook-Pro-3.local>
2020-04-30 12:30:04 +0100

    script n fsfs

--- code/fsf_maker_group_rs-sbfc.py
+++ code/fsf_maker_group_rs-sbfc.py
@@ -6,7 +6,9 @@ Created on Fri Apr 10 10:38:41 2020
 @author: willstrawson
 """
 import pandas as pd 
-
+import glob 
+import os
+import numpy as np
 # This script will create the fsf files needed for group level rs-sbfc analysis in fsl
 # It will extract avh metrics, mwf scores and symtom scores for each patient and populate an fsf template 
 
@@ -26,18 +28,65 @@ dff = dfff.rename({'MeanAVHduration':'avh_meandur', 'total_avh_duration':'avh_to
 # keep only columns which will be used as a regressor, just for tidyness 
 cols = ['patient_run','patient','run','avh_instances','avh_meandur','avh_totaldur',
          'MWF1','MWF2','MWF3','MWF4','MWF5','MWF6','AVHF1','AVHF2',
-         'AVHF1_2','AVHF2_2','BSIS_V','BAVQ_P']
+         'AVHF1_2','AVHF2_2','BAVQ_P', 'avh_pca']
 
 df = dff[cols]
 
+# Add Age and BSIS_v column from per_patient dataframe to include as regressor 
+# If you want to include Any perpatient symptoms, now's the time... its easy!
+agedf = pd.read_csv('/Users/willstrawson/Documents/PhD/Rotation_2/Data/ViBE_age_bsisv_sdt.csv')
+agedf.rename(columns={'BSIS_Voices':'BSIS_V'})
 
+# Merge age df into main df
+df = pd.merge(df, agedf, on='patient', how='outer')
+
+
+# Now to read in framewise displacement  
+fds = {}
+fd_fls = glob.glob('/Users/willstrawson/Documents/PhD/Rotation_2/fmriprep_regressors/*')
+for i in fd_fls:
+    fds[os.path.split(i)[1][4:10]] = pd.read_csv(i, sep='\t')
+# Keep only fd 
+cols2 =  ['framewise_displacement']
+for i in fds:
+    fds[i] = fds[i][cols2]
+    # Take the mean 
+    fds[i] = fds[i].mean()
+    # renamecolum
+    fds[i].rename(columns={'0':'mean_fd'})
+
+# Make these individual Series into one dataframe where columns = patient mean_fd 
+mean_fd = pd.DataFrame(fds).T
+# Reset index so patient number because normal column
+mean_fd = mean_fd.reset_index()
+# Rename patient column
+mean_fd.rename(columns={'index':'patient'}, inplace=True)
+#Turn patient column into int
+mean_fd= mean_fd.apply(pd.to_numeric, errors='coerce')
+# merge this dataframe with the main one :-)
+df = pd.merge(df, mean_fd, on='patient', how='outer')
+
+#Turn whitespzce into NaN
+df = df.replace(r'^\s*$', np.nan, regex=True)
+# Turn all numbers to numbers 
 df[cols[1:]] = df[cols[1:]].apply(pd.to_numeric, errors='coerce')
+#Do the following columns individually as the abovemethjod did not work 
+df['Age']=pd.to_numeric(df['Age'], errors='coerce')
+df['BSIS_Voices']=pd.to_numeric(df['BSIS_Voices'], errors='coerce')
+df['SDT_criterion']=pd.to_numeric(df['SDT_criterion'], errors='coerce')
 
 # replace NaNsand create new imputed df
 dfi = df.fillna(df.mean())
 
+# TODO: output statistics on how many missing values were imputed
+
+
 # Demean columns
-dfi[cols[3:]] = dfi[cols[3:]].sub(dfi[cols[3:]].mean(), axis='columns')
+allcols = df.columns.tolist()
+dfi[allcols[3:]] = dfi[allcols[3:]].sub(dfi[allcols[3:]].mean(), axis='columns')
+
+# rename framewise displacement column 
+dfi.rename(columns={'framewise_displacement':'mean_fd'},inplace=True)
 
 # make column headers lowercase
 dfi.columns = [x.lower() for x in dfi.columns]
@@ -45,13 +94,9 @@ dfi.columns = [x.lower() for x in dfi.columns]
 # remove patient 54665 because they're not in analysis and mess up indexing
 dfi = dfi[dfi.patient != 54665]
 
+
+
 # ------ #
-
-#Read in fsf template
-template = '/Users/willstrawson/Documents/PhD/Rotation_2/fsfs/template_rest/pretemplate_group.fsf'
-# Specify name of output fsf
-fsf_out = '/Users/willstrawson/Documents/PhD/Rotation_2/fsfs/template_rest/rs-sbfc_group_template.fsf'
-
 
 # Need to engineer search terms that appear in fsf template
 # These are uppercase column names + _ sub number
@@ -67,10 +112,9 @@ for i in searchterms:
         srchs.append(str(i) + '_' + str(n))  
         
 # Create replacements dict where key = searchterms and value is number from dfi
-
 replacements = {}
 for i in srchs:
-    # reenineer column header name from search terms
+    # reegnineer column header name from search terms
     var = i[:-7].lower()
     print (var)
     # ... and the same for participant name
@@ -78,7 +122,12 @@ for i in srchs:
     print (ps)
     # Add the specific values for each patient and variable
     replacements[i] = dfi[var].loc[dfi['patient'] == int(ps)]
-    
+
+#Read in fsf template
+template = '/Users/willstrawson/Documents/PhD/Rotation_2/fsfs/template_rest/rs-sbfc_group_mwfs_template.fsf'
+# Specify name of output fsf
+fsf_out = '/Users/willstrawson/Documents/PhD/Rotation_2/fsfs/template_rest/rs-sbfc_group_mwfs.fsf'
+
 with open (template, 'r') as infile:
     with open (fsf_out, 'w') as outfile:
         for line in infile:
@@ -87,7 +136,6 @@ with open (template, 'r') as infile:
                 #print (src, target)
                 line = line.replace(str(src),str(target.to_numpy()[0]))
             outfile.write(line)
-
 
 
 
